666fad060c31371b52e72e7d96e56206e5adefae
MOSTAFA <allahmmo>
2021-09-16 10:40:19 -0400

    Getting inputs from the user for ProfileFragment

--- app/src/main/java/fingertiptech/medontime/ui/profile/ProfileFragment.java
+++ app/src/main/java/fingertiptech/medontime/ui/profile/ProfileFragment.java
@@ -1,10 +1,14 @@
 package fingertiptech.medontime.ui.profile;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,19 +22,39 @@ import fingertiptech.medontime.ui.profile.ProfileViewModel;
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
+    EditText editText_username;
+    EditText getEditText_password;
+    Button btn_signIn;
+    Button btn_register;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         profileViewModel =
                 new ViewModelProvider(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-//        final TextView textView = root.findViewById(R.id.text_profile);
-//        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+
+        editText_username = root.findViewById(R.id.editText_userName);
+        getEditText_password = root.findViewById(R.id.editText_Password);
+        btn_signIn = root.findViewById(R.id.btnSignIn);
+        btn_register = root.findViewById(R.id.btnRegister);
+
+        btn_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editText_username.getText().toString();
+                String password = getEditText_password.getText().toString();
+                Log.wtf("Username: ", username+"\n");
+                Log.wtf("Password: ", password);
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.wtf("Register", "Register is clicked");
+            }
+        });
+
         return root;
     }
 }
