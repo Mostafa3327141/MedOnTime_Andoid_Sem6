de6cc2220a6c0fde47f3f20f6279f4ddf74d622f
sinaxace <sinazace@zohomail.com>
2021-09-17 12:40:57 -0400

    Committing nav setup with NFC adaptor part 2.

--- app/src/main/java/fingertiptech/medontime/ui/scanNFCtag/ScanNFCTagViewModel.java
+++ app/src/main/java/fingertiptech/medontime/ui/scanNFCtag/ScanNFCTagViewModel.java
@@ -1,7 +1,18 @@
 package fingertiptech.medontime.ui.scanNFCtag;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class ScanNFCTagViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<String> mText;
+
+    public ScanNFCTagViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Scan NFC Tag Fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
