ff46c6bdcd72da61f27f5a2513ae8d9e2cd4e253
sinaxace <sinazace@zohomail.com>
2021-10-13 17:01:46 -0400

    Adding step 3 nfc fragment code manually to master branch.

--- app/src/main/java/fingertiptech/medontime/ui/medicine/MedicineFragmentStep3.java
+++ app/src/main/java/fingertiptech/medontime/ui/medicine/MedicineFragmentStep3.java
@@ -2,6 +2,7 @@ package fingertiptech.medontime.ui.medicine;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -16,7 +17,26 @@ import fingertiptech.medontime.R;
 
 public class MedicineFragmentStep3 extends Fragment {
 
-    private MedicineFragmentStep3ViewModel mViewModel;
+    private MedicineViewModel medicineViewModel;
+
+    MedicineFragmentStep3.OnDataPass dataPasser;
+
+    // for passing data to activity
+    public interface OnDataPass {
+        public void onDataPass(String data, View view);
+    }
+
+    // connecting the containing class' implementation of the interface to the fragment in the onAttach method
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        dataPasser = (OnDataPass) context;
+    }
+
+    // for when you need to handle the passing of data in this fragment
+    public void passData(String data, View view) {
+        dataPasser.onDataPass(data, view);
+    }
 
     public static MedicineFragmentStep3 newInstance() {
         return new MedicineFragmentStep3();
@@ -25,14 +45,24 @@ public class MedicineFragmentStep3 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_medicine_step3_nfc_scan, container, false);
-    }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(MedicineFragmentStep3ViewModel.class);
-        // TODO: Use the ViewModel
+        System.out.println("See me??");
+        System.out.println(MedicineFragment.resultQRScan);
+        medicineViewModel =
+                new ViewModelProvider(this).get(MedicineViewModel.class);
+
+        if(MedicineFragment.resultQRScan != null){
+            medicineViewModel.init(MedicineFragment.resultQRScan);
+            medicineViewModel.getMedicationRepository().observe(getViewLifecycleOwner(), medicationsResponse -> {
+                System.out.println(medicationsResponse.getId());
+                //imageViewMedication.setImageBitmap(convertBase64ToBitmap(medicationsResponse.getMedicationImage()));
+            });
+        }
+
+
+        View view = inflater.inflate(R.layout.fragment_medicine_step3_nfc_scan, container, false);
+        passData("NFC Fragment Active", view);
+        return view;
     }
 
 }
