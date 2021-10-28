297d4109972f17af2f3b51147518bd8f8aedfeec
MOSTAFA <allahmmo>
2021-10-28 10:51:42 -0400

    Notification and Alarm classes added

---
+++ app/src/main/java/fingertiptech/medontime/NotificationAndAlarm.java
@@ -0,0 +1,48 @@
+package fingertiptech.medontime;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class NotificationAndAlarm extends Application {
+
+    public static final String CHANNEL_ID = "NotificationChannel";
+    public static final String ALARM_ID = "AlarmChannel";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        
+        createNotificationChannel();
+        createAlarmChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Medication Reminder Notification",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            notificationChannel.setDescription("It is time to take your medication");
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private void createAlarmChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel alarmChannel = new NotificationChannel(
+                    ALARM_ID,
+                    "Medication Reminder Alarm",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            alarmChannel.setDescription("This is time to take your medication");
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(alarmChannel);
+        }
+    }
+}
