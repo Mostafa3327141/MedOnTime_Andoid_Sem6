d80829ea0145ae6a9a9e35c2b31296f8b1a66168
MOSTAFA <allahmmo>
2021-10-31 10:38:24 -0400

    Commit before merging

--- app/src/main/java/fingertiptech/medontime/ui/medicine/MedicineFragment.java
+++ app/src/main/java/fingertiptech/medontime/ui/medicine/MedicineFragment.java
@@ -7,7 +7,6 @@ import android.Manifest;
 import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.PendingIntent;
-import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -27,7 +26,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
